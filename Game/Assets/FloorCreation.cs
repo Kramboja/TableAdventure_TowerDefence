c4adc8aaf96b84756db385adf10771009f7eceb7
Kramboja <koen5067@icloud.com>
2015-11-09 10:18:13 +0100

    PlayerMovement + map Creation

---
+++ Game/Assets/FloorCreation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorCreation : MonoBehaviour {
+
+	private int rows = 25;
+	private int lines = 25;
+	public Transform Floor;
+	public Transform Tree;
+	private int baseSize = 3;
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		//maakt een veld aan van het aantal rows x het aantal lines
+		for(int i = 0;i < lines; i++)
+		{
+			for(int j = 0; j < rows; j++)
+			{
+				Transform trans = (Transform)Instantiate(Floor, new Vector3(i*10f, -0.5f,j*10f), transform.rotation);
+				trans.parent = transform;
+				
+				/*if(i >= lines/2 - baseSize && i <= lines/2 + baseSize)
+				{
+					if(j == rows/2 - baseSize || j == rows/2 + baseSize)
+					{
+						Transform cityWall = (Transform)Instantiate(wall, new Vector3(i*10,4,j*10), Quaternion.Euler(0,0,90*Random.Range(0,4)));
+					}
+				}
+*/
+				/*				if(j >= rows/2 - baseSize + 1 && j <= lines/2 + baseSize - 1)
+				{
+					if(i == lines/2 - baseSize || i == lines/2 + baseSize)
+					{
+						Transform cityWall = (Transform)Instantiate(wall, new Vector3(i*10,4,j*10), Quaternion.Euler(0,0,90*Random.Range(0,4)));
+					}
+				}
+*/
+				
+				if(i < 3 || j < 4 || i > lines - 4 || j > rows - 4)
+				{
+					Transform tree = (Transform)Instantiate(Tree, new Vector3(Random.Range(-5, 5) + i * 10, 3f, Random.Range(-5, 5) + j * 10),Quaternion.Euler(0,Random.Range(1,360),0));
+					tree.parent = transform;
+				}
+			}
+		}
+	}
+}
---
+++ Game/Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	private float speed 			= 5.0f;
+	private float mouseSensivity 	= 10f;
+	private float verticalRotation 	= 0;
+
+	private float upDownRange 		= 60f;
+	
+	void Awake()
+	{
+		Screen.lockCursor = true;
+	}
+
+	void Update () 
+	{
+		if (Input.anyKey)
+			buttonPressed ();
+		
+		float rotHorizontal = Input.GetAxis ("Mouse X") * mouseSensivity;
+		transform.Rotate (0, rotHorizontal, 0);
+		
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensivity;
+		verticalRotation = Mathf.Clamp (verticalRotation, -upDownRange, upDownRange);
+		
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
+	}
+	
+	private void buttonPressed()
+	{	
+		if (Input.GetKey(KeyCode.Escape) && Screen.lockCursor)
+			Screen.lockCursor = false;
+		else
+			Screen.lockCursor = true;
+		
+		float translationV 	= Input.GetAxis ("Vertical") 	* speed;
+		float translationH	= Input.GetAxis ("Horizontal") 	* speed;
+		
+		translationV 		*= Time.deltaTime;
+		translationH 		*= Time.deltaTime;
+		
+		transform.Translate (translationH, 0, translationV);
+	}
+}
