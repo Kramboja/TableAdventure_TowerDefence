a999cefda9cfaedf4d754cc1a2bda48fb7e35fd1
Kramboja <koen5067@icloud.com>
2015-11-09 09:32:30 +0100

    SplashScreen Finished

---
+++ Game/Assets/_Scripts/PlayMovie.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour {
+
+	void Start () 
+	{
+		Renderer r = GetComponent<Renderer>();
+		MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+		movie.Play();
+
+		Invoke("StartGame",3f);
+	}
+	
+	private void StartGame()
+	{
+		Application.LoadLevelAsync("Menu");
+	}
+}
